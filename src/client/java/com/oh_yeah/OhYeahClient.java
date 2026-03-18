f10064a51f8837cbbaf026015d43b0ee8b4b5ff9
linglingshui <nothings@new>
2026-03-18 19:59:55 +0800

    Initial commit: Minecraft Mod 1.21.1 基础框架

---
+++ src/client/java/com/oh_yeah/OhYeahClient.java
@@ -0,0 +1,10 @@
+package com.oh_yeah;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OhYeahClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
